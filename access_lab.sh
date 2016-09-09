451ed7515d29f2abe4520704ab5ccbc56f0fa29b
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2016-09-09 08:51:39 +0200

    format changes

--- access_lab.sh
+++ access_lab.sh
@@ -2,7 +2,8 @@
 
 docker run -it -p 127.0.0.1:8015:8000 \
        --user="$(id -u):$(id -g)" \
-       -v $(pwd):/home/lab felix11h/aniso_netw_env \
+       -v $(pwd):/home/lab \
+       felix11h/aniso_netw_env \
        /bin/bash -c \
        'source /home/docker/env/neurosci/bin/activate;
         screen -d -m /home/docker/env/neurosci/bin/smtweb --allips;
