69da17948d2cddd7c78147f12653f2fb45863970
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2016-09-10 20:15:46 +0200

    added docstrings

--- comp/functions/aniso_networks.py
+++ comp/functions/aniso_networks.py
@@ -15,10 +15,14 @@ def rotate(alpha, positions):
 
 def find_axon_targets(i, alpha, positions, w):    
     '''
-    i: index of source neuron
-    alpha: axon direction
-    positions: matrix of positions
-    w: function returning the axon width/2
+    Finds the target nodes for a neuron with index "i", 
+    axon orientation "alpha", axon width function "w" 
+    in a network with neuros located at "positions".
+    --------------------------------------------------
+    i:           index of source neuron
+    alpha:       axon direction
+    positions:   array of node positions
+    w:           function returning the axon width/2
     '''
 
     targets = []    
@@ -32,16 +36,30 @@ def find_axon_targets(i, alpha, positions, w):
     return targets
 
 
-def generate_aniso_network(N, w, ed_l = 212., save_path = ''):
+def generate_aniso_network(N, w, ed_l, save_path = ''):
     '''
+    Generates a (tuned) anisotropic network.
+    --------------------------------------------------
+    N:    Number of neurons
+    w:    Axon-width function. Takes a single value x, 
+          the axon distance, and returns the half-width of 
+          the "axon-band" at this length.
+          If w returns a single value (w = lambda x: c),
+          the network is refered to as "anisotropic", 
+          otherwise "tuned anisotropic".
+    ed_l: Edge length of the quadratic surface area of 
+          the network
     '''
     
     g = gt.Graph()
     
     # store graph attributes
     graph_type = g.new_graph_property("string")
-    graph_type[g] = "anisotropic"
-    g.graph_properties["graph_type"] = graph_type   
+    if w(0)==w(ed_l/2.)==w(ed_l):
+        graph_type[g] = "aniso"
+    else:
+        graph_type[g] = "tuned_aniso"
+    g.graph_properties["graph_type"] = graph_type
 
     edge_length = g.new_graph_property("double")
     edge_length[g] = ed_l
