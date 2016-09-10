bd62d2c67902e00fd9ef3c223a3137bf84854c07
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2016-09-10 16:05:40 +0200

    get xy positions of vertices

--- comp/functions/network_eval.py
+++ comp/functions/network_eval.py
@@ -10,3 +10,27 @@ def eval_connectivity(g):
     denom = g.num_vertices()*(g.num_vertices()-1.)
     
     return numer/denom
+
+
+def get_xy(g):
+    '''
+    return x and y coordinates of the vertices
+    '''
+
+    xy = g.vertex_properties["xy"]
+
+    xs = [xy[v][0] for v in g.vertices()]
+    ys = [xy[v][1] for v in g.vertices()]
+
+    return xs,ys
+
+
+def get_target_ids(g, i):
+
+    source = g.vertex(i)
+    targets = []
+
+    for e in source.out_edges():
+        targets.append(int(e.target()))
+
+    return targets
--- comp/test/test_network_eval.py
+++ comp/test/test_network_eval.py
@@ -5,7 +5,13 @@ import numpy as np
 
 import graph_tool as gt
 
-from functions import eval_connectivity 
+# get tested
+from functions import ( eval_connectivity,
+                        get_xy,
+                        get_target_ids )
+
+# functions assisting in testing
+from functions import ( generate_aniso_network )
 
 
 class Test_eval_connectivity(unittest.TestCase):
@@ -17,8 +23,31 @@ class Test_eval_connectivity(unittest.TestCase):
     g.add_edge(g.vertex(2), g.vertex(0))
 
     def test_correct_connectivity(self):
-        self.assertEqual(eval_connectivity(self.g),
-                         0.5)       
+        self.assertEqual(eval_connectivity(self.g), 0.5)       
 
+
+class Test_get_xy(unittest.TestCase):
+
+    g = generate_aniso_network(50, lambda x: 1.,
+                               ed_l = 100.)
+
+    def test_number_of_xy_match(self):
+        xs, ys = get_xy(self.g)
+        self.assertEqual(len(xs), 50)
+        self.assertEqual(len(ys), 50)
+
+    def test_xy_in_correct_range(self):
+        xs, ys = get_xy(self.g)
+        for x in xs:
+            self.assertGreaterEqual(x, 0.)
+            self.assertLess(x, 100.)
+        for y in ys:
+            self.assertGreaterEqual(y, 0.)
+            self.assertLess(y, 100.)
+
+
+#class Test_get_target_ids(unittest.TestCase):
+    
+        
 if __name__ == '__main__':
     unittest.main()
