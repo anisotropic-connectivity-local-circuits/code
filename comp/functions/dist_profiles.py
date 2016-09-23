6541356913a1b3c0c0fe84ed9449fbebdde806a7
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2016-09-23 16:53:08 +0200

    reformat into class

--- comp/functions/dist_profiles.py
+++ comp/functions/dist_profiles.py
@@ -2,20 +2,33 @@
 import numpy as np
 
 
-def aniso_netw_dist_profile(x, w):
+class Aniso_netw_dist_profile(object):
     '''
-    Distance-dependent connection probability profile of 
-    the anisotropic network with axon width 2w as derived 
-    in the supporting information
-    --------------------------------------------------
-    x  : inter-neuron distance
-    w  : half-width of the axon
+    Struct to implement distance-dependent connection 
+    probability profile. Initialize with axon half-width
+    w and use C(x) to get the connection probability at 
+    inter-neuron distance x.
     '''
 
-    assert(x>=0)
+    def __init__(self, w):
+        self.w = w
+        
+    def C(self,x):
+        '''
+        Distance-dependent connection probability profile of 
+        the anisotropic network with axon width 2w as derived 
+        in the supporting information
+        --------------------------------------------------
+        x  : inter-neuron distance
+        w  : half-width of the axon
+        '''
+        
+        assert(x>=0)
 
-    if x < w:
-        return 0.5
-    else:
-        return 1/(np.pi) * np.arcsin(2*w/(2*x))
+        if x < self.w:
+            return 0.5
+        else:
+            return 1/(np.pi) * np.arcsin(2*self.w/(2*x))
+
+
         
--- comp/test/test_dist_profiles.py
+++ comp/test/test_dist_profiles.py
@@ -4,10 +4,10 @@ import numpy as np
 import graph_tool as gt
 
 # get tested
-from functions import ( aniso_netw_dist_profile )
+from functions import ( Aniso_netw_dist_profile )
 
 
-class Test_aniso_netw_dist_profile(unittest.TestCase):
+class Test_Aniso_netw_dist_profile(unittest.TestCase):
 
     def test_profile(self):
         raise NotImplementedError
