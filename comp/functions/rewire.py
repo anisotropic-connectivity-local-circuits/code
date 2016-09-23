3f2c067f89e9dcb683276d5ad9509ad5d2510997
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2016-09-23 17:50:27 +0200

    add rewire methods

---
+++ comp/functions/rewire.py
@@ -0,0 +1,18 @@
+
+import graph_tool as gt
+import numpy as np
+
+def rewire(g, fraction=1, eps_f=0.01):
+
+    '''
+
+    --------------------------------------------------
+    g        : graph to rewire
+    fraction : fraction of edges that get rewired
+    eps_f    : margin for rewiring specified as 
+               a fraction of the graph's ed_l
+    '''
+
+    pass
+    
+
---
+++ comp/test/test_rewire.py
@@ -0,0 +1,20 @@
+
+import unittest, os
+import numpy as np
+
+
+# get tested
+from functions import ( rewire )
+
+# functions assisting in testing
+# from functions import (eval_connectivity)
+
+        
+class Test_rewire(unittest.TestCase):
+
+    def test_rewire(self):
+        raise NotImplementedError
+        
+
+if __name__ == '__main__':
+    unittest.main()
