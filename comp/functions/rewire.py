8232384c895c300d72a974dd2c7fbb9d408fb46e
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2017-04-26 17:49:49 +0200

    rewiring implemented & tested

--- comp/functions/rewire.py
+++ comp/functions/rewire.py
@@ -1,11 +1,14 @@
 
 import graph_tool as gt
 import numpy as np
+import random
 
-def rewire(g, fraction=1, eps_f=0.01):
+from functions import get_dist_matrix
 
+def rewire(g, rew_frac=1., eps_f=0.01):
     '''
-
+    Rewire a given graph while preserving the 
+    distance-dependent connectivity profile
     --------------------------------------------------
     g        : graph to rewire
     fraction : fraction of edges that get rewired
@@ -13,6 +16,76 @@ def rewire(g, fraction=1, eps_f=0.01):
                a fraction of the graph's ed_l
     '''
 
-    pass
+    ed_l = g.graph_properties["ed_l"]
+    eps = ed_l * eps_f
+
+    h = gt.Graph(g)
+    h.clear_edges()
+
+    D = get_dist_matrix(g)
+
+    rew_stat = {}
+    fail_edges = []
+
+    for v in g.vertices():
+
+        to_rewire = []
+        remain_unchanged = []
+
+        for out_e in v.out_edges():
+
+            if np.random.uniform() <= rew_frac:    
+                to_rewire.append(out_e)
+            else:
+                remain_unchanged.append(out_e)
+
+        # connect unchanged edges first, those are not
+        # suitable targets for rewiring anymore
+        for out_e in remain_unchanged:
+            h.add_edge(v, out_e.target())
+
+        for out_e in to_rewire:
+
+            #find suitable new target
+            d = D[int(out_e.source()),int(out_e.target())]
+            rew_opts = np.where(
+                np.logical_and((d-eps) <= D[int(v)],
+                               D[int(v)] <= (d+eps)))[0]
+
+            rew_stat[(int(v), int(out_e.target()))]= len(rew_opts)
+
+            new_target = random.choice(rew_opts)
+
+            target_valid = True
+            rew_tries = 0
+
+            while (bool(h.edge(v,h.vertex(new_target))) or
+                   int(v)==new_target):
+            # if edge already exists, whether because it was an
+            # unchanged edge (rew_frac < 1)or it has been chosen as
+            # a rewiring target for the same v, or v is supposed to
+            # connect to itsef (can only happen if d < eps), chose
+            # other new target
+
+                rew_tries += 1
+                if rew_tries > len(rew_opts)/2.:
+                    fail_edges.append((int(v), int(out_e.target())))
+                    target_valid = False
+                    break
+
+                new_target = random.choice(rew_opts)
+
+            if target_valid:
+                h.add_edge(h.vertex(int(v)),h.vertex(new_target))
     
+
+         
+    print "Failed to rewire %d edges" %(len(fail_edges))
+
+    statistics = {"rew_stat":rew_stat, "fail_edges":fail_edges}
+
+    h.graph_properties["rewired"] = h.new_graph_property("bool",
+                                                         True)
+    
+    return h, statistics    
 
--- comp/test/test_rewire.py
+++ comp/test/test_rewire.py
@@ -2,19 +2,81 @@
 import unittest, os
 import numpy as np
 
+import graph_tool as gt
+import graph_tool.topology
 
 # get tested
 from functions import ( rewire )
 
-# functions assisting in testing
-# from functions import (eval_connectivity)
+# functions assisting in testin
+from functions import ( generate_aniso_network )
+
 
         
 class Test_rewire(unittest.TestCase):
 
-    def test_rewire(self):
-        raise NotImplementedError
+    g = gt.Graph()
+    ed_l = 100
+    
+    edge_length = g.new_graph_property("double")
+    edge_length[g] = ed_l
+    g.graph_properties["ed_l"] = edge_length
+
+    xy = g.new_vertex_property("vector<double>")
+    g.vertex_properties["xy"] = xy
+
+    def test_single_target_results_in_isomorphic_graph(self):
+        # construct with a single edge that cannot be rewired
+        h = gt.Graph(self.g)
+        h.add_vertex(2)
+        h.add_edge(h.vertex(0), h.vertex(1))
+        xy = h.vertex_properties["xy"]
+        xy[h.vertex(0)] = (50,50)
+        xy[h.vertex(1)] = (55,55)
+        k, stat = rewire(h)
+        self.assertTrue(gt.topology.isomorphism(h,k))
+
+    def test_rew_fraction_zero_results_in_isomorphic_graph(self):
+        h = generate_aniso_network(100, lambda x: 12.6, 100)
+        k, stat = rewire(h, rew_frac=0)
+        self.assertTrue(gt.topology.isomorphism(h,k))
+
+    def test_only_possible_targets_get_rewired(self):
+        h = gt.Graph(self.g)
+        h.add_vertex(2)
+        h.add_edge(h.vertex(0), h.vertex(1))
+        xy = h.vertex_properties["xy"]
+        xy[h.vertex(0)] = (50,50)
+        xy[h.vertex(1)] = (55,55)
+        k, stat = rewire(h)
+        self.assertTrue(gt.topology.isomorphism(h,k))
         
+    def test_after_rewiring_rew_property_is_true(self):
+        h = gt.Graph(self.g)
+        h, stat = rewire(h)
+        self.assertEqual(h.graph_properties["rewired"],True)
+
+    g_aniso = generate_aniso_network(1000, lambda x: 12.6, 100)
+    # g_aniso = gt.Graph(g)
+    # g_aniso.add_vertex(2)
+    # g_aniso.add_edge(g_aniso.vertex(0), g_aniso.vertex(1))
+    # g_aniso.add_edge(g_aniso.vertex(0), g_aniso.vertex(1))
+        
+    # we expect the graph to have no self loops
+    def test_graph_self_loops(self):
+        ne_before = self.g_aniso.num_edges()
+        gt.stats.remove_self_loops(self.g_aniso)
+        ne_after = self.g_aniso.num_edges()
+        self.assertEqual(ne_before, ne_after)
+
+    # we expect the graph to have no parallel edges
+    def test_graph_parallel_edges(self):
+        ne_before = self.g_aniso.num_edges()
+        gt.stats.remove_parallel_edges(self.g_aniso)
+        ne_after = self.g_aniso.num_edges()
+        self.assertEqual(ne_before, ne_after)
+
+
 
 if __name__ == '__main__':
     unittest.main()
