1251cdbc212b4dfecc40f35006a35b82f41cfaec
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2017-04-27 21:04:02 +0200

    rew_stats -> rew_stat

--- comp/get_rewiring_stats_label.py
+++ comp/get_rewiring_stats_label.py
@@ -1,7 +1,7 @@
 
 from params.get_rewiring_stats_params import *
 
-label = 'rew-stats_aniso_rf{:.2f}_ef{:.2f}'.format(rew_frac, eps_f)
+label = 'rew-stat_aniso_rf{:.2f}_ef{:.2f}'.format(rew_frac, eps_f)
 
 if __name__ == "__main__":
     print label
