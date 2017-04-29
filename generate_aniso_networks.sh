26f74b10e8ec9b883022d197ca1ab4cc3a8ceb26
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2017-04-29 16:38:37 +0200

    rm .sh scripts from repository

--- generate_aniso_networks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash         
-
-name='generate_aniso_networks';
-label=`python generate_aniso_networks_label.py` ;
-
-smt run --executable=/home/docker/env/neurosci/bin/python --main=comp/$name.py --reason='generate aniso networks' --label=$label --tag=aniso,netw params/$name\_params.py
-
-cp params/$name\_params.py data/$label\_params.py
-cp $BASH_SOURCE data/$label"_run.sh"
