cd1c7c9b9cab9a63f336fb1b3eb9ffde244e2bc7
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2017-04-29 16:35:28 +0200

    remove .sh scripts from version control

--- generate_aniso_networks.sh
+++ generate_aniso_networks.sh
@@ -1,9 +1,9 @@
 #!/bin/bash         
 
 name='generate_aniso_networks';
-label=`python comp/generate_aniso_networks_label.py` ;
+label=`python generate_aniso_networks_label.py` ;
 
-smt run --executable=/home/docker/env/neurosci/bin/python --main=comp/$name.py --reason='generate aniso networks' --label=$label --tag=aniso,netw comp/params/$name\_params.py
+smt run --executable=/home/docker/env/neurosci/bin/python --main=comp/$name.py --reason='generate aniso networks' --label=$label --tag=aniso,netw params/$name\_params.py
 
-cp comp/params/$name\_params.py data/$label\_params.py
+cp params/$name\_params.py data/$label\_params.py
 cp $BASH_SOURCE data/$label"_run.sh"
