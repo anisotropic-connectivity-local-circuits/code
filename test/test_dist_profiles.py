840a0e115fcd977beb3d1960dc98d0a2c6d8fb0f
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2017-05-06 22:28:35 +0200

    test dist profiles via expected connectivity

--- test/test_dist_profiles.py
+++ test/test_dist_profiles.py
@@ -6,12 +6,25 @@ import graph_tool as gt
 # get tested
 from comp.functions import ( Aniso_netw_dist_profile )
 
+# functions assisting in testing
+from comp.functions import ( generate_dist_network,
+                             eval_connectivity )
+
 
 class Test_Aniso_netw_dist_profile(unittest.TestCase):
 
-    def test_profile(self):
-        #raise NotImplementedError
-        self.assertTrue(False)
+
+    def test_expected_connectivity(self):
+        an_ddcp = Aniso_netw_dist_profile(37.3)
+        N, ed_l = 1000, 296
+        mu_list = []
+        for k in range(3):
+            h = generate_dist_network(N, an_ddcp.C, ed_l)        
+            mu_list.append(eval_connectivity(h))
+        mu = np.mean(mu_list)
+        # expected 0.116 +- 0.05
+        self.assertGreaterEqual(mu, 0.111)
+        self.assertLessEqual(mu, 0.121)
 
    
 
