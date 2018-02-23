d18048db0c9025605ca3431cb7fb25af07bb0fc4
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2018-02-23 19:08:34 +0100

    sample three motif script & label

---
+++ sample_three_motifs.py
@@ -0,0 +1,31 @@
+
+from functions import sample_three_motifs
+import graph_tool as gt
+
+from params.sample_three_motifs_params import *
+from sample_three_motifs_label import label
+
+import pickle, os, sys
+
+data = {}
+for gpath in sys.argv[1:]:
+    g = gt.load_graph(gpath)
+    motif_counts = sample_three_motifs(g, sample_size)
+    gid = os.path.splitext(os.path.basename(gpath))[0][-4:]
+
+    data[gid]=motif_counts
+
+    fpath = "data/three_motif_counts_" \
+            + "{:s}_S{:d}.p".format(graph_type, sample_size)
+
+    with open(fpath, "wb") as pfile:
+        pickle.dump(data, pfile)
+    
+
+
+    
+    
+
+
+
+    
---
+++ sample_three_motifs_label.py
@@ -0,0 +1,8 @@
+
+from params.sample_three_motifs_params import *
+
+label = "sample_three_motifs_{:s}_S{:d}".format(graph_type, sample_size)
+
+if __name__ == "__main__":
+    print label
+
