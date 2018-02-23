7ef52eda55e4c4954603098a33c6056c11f7ed2e
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2018-02-23 16:09:09 +0100

    motif list gets counted correctly

--- functions/three_motifs.py
+++ functions/three_motifs.py
@@ -1,30 +1,41 @@
-
+# -*- coding: utf-8 -*-
 
 import graph_tool as gt
 import graph_tool.stats
+import graph_tool.topology
+
+import numpy as np
 
 
-def generate_three_motif_list():
-    
+def generate_three_motif_list(with_empty):
+    '''
+    Does not contain first motif without edges!
+    '''
 
     graph_list = []
 
-    if mode == "full":
+    if with_empty:
 
+        #      o
+        #   ↙↗ ↘↖  
+        #  o   ⇄  o
         g = gt.Graph()
         g.add_vertex(3)
         graph_list.append(g)
 
-        g = gt.Graph()
-        g.add_vertex(3)
-        g.add_edge(g.vertex(0),g.vertex(1))
-        graph_list.append(g)
+    #      o
+    #   ↙↗ ↘↖  
+    #  o   ⇄  o
+    g = gt.Graph()
+    g.add_vertex(3)
+    g.add_edge(g.vertex(0),g.vertex(1))
+    graph_list.append(g)
 
-        g = gt.Graph()
-        g.add_vertex(3)
-        g.add_edge(g.vertex(2),g.vertex(0))
-        g.add_edge(g.vertex(0),g.vertex(2))
-        graph_list.append(g)
+    g = gt.Graph()
+    g.add_vertex(3)
+    g.add_edge(g.vertex(2),g.vertex(0))
+    g.add_edge(g.vertex(0),g.vertex(2))
+    graph_list.append(g)
 
 
     g = gt.Graph()
@@ -114,9 +125,9 @@ def generate_three_motif_list():
     g.add_edge(g.vertex(0),g.vertex(1))
     graph_list.append(g)
 
-    g = gt.Graph()
-    g.add_vertex(3)
-    g.add_vertex(3)
+    # g = gt.Graph()
+    # g.add_vertex(3)
+    # g.add_vertex(3)
     g = gt.Graph()
     g.add_vertex(3)
     g.add_edge(g.vertex(0),g.vertex(1))
@@ -127,7 +138,6 @@ def generate_three_motif_list():
     g.add_edge(g.vertex(2),g.vertex(1))
     graph_list.append(g)
 
-    
     return graph_list
 
 
@@ -146,46 +156,53 @@ def sample_three_motifs(g, sampling_size):
     ne_after = g.num_edges()
     assert(ne_before == ne_after)
 
-    # song_motif_list = ng.evaluation.song_motif_graph_list()
-    # motif_counts = np.zeros(len(song_motif_list))    
+    three_motif_list = generate_three_motif_list(with_empty=False)
+    motif_counts = np.zeros(len(three_motif_list)+1)    
     
-    # N = g.num_vertices()
+    N = g.num_vertices()
 
-    # for k in range(sampling_size):
+    for k in range(sampling_size):
 
-    #     vertex_ids = np.array([])
-    #     while len(np.unique(vertex_ids)) < 3:
-    #         vertex_ids = np.random.randint(0,N,3)
+        vertex_ids = np.array([])
+        while len(np.unique(vertex_ids)) < 3:
+            vertex_ids = np.random.randint(0,N,3)
 
-    #     is_part_of_motif = g.new_vertex_property("bool")
+        is_part_of_motif = g.new_vertex_property("bool")
+              
+        for v_id in vertex_ids:
+            is_part_of_motif[g.vertex(v_id)] = True
 
-    #     for v_id in vertex_ids:
-    #         is_part_of_motif[g.vertex(v_id)] = True
+        g.set_vertex_filter(is_part_of_motif)
 
-    #     g.set_vertex_filter(is_part_of_motif)
+        motif_matches = []
 
-    #     motif_matches = []
-
-    #     for motif in song_motif_list:
-    #         if gt.topology.isomorphism(g,motif):
-    #             motif_matches.append(1)
-    #         else:
-    #             motif_matches.append(0)
+        # test for motif without edges separately as 
+        # graph_tool.topology does not handle it
+        if g.num_edges()==0:
+            motif_matches = [1]+[0]*len(three_motif_list)                            
+        else:
+            motif_matches.append(0)
+          
+            for motif in three_motif_list:
+                if gt.topology.isomorphism(g,motif):
+                    motif_matches.append(1)
+                else:
+                    motif_matches.append(0)
     
-    #     if sum(motif_matches) == 0:
-    #         print "Error, no matching motif found."    
+        if sum(motif_matches) == 0:
+            print "Error, no matching motif found."    
             
-    #     elif sum(motif_matches) > 1:
-    #         print "Error, more than 1 motif match."
+        elif sum(motif_matches) > 1:
+            print "Error, more than 1 motif match."
         
-    #     motif_counts += np.array(motif_matches)
+        motif_counts += np.array(motif_matches)
 
-    #     g.set_vertex_filter(None)
+        g.set_vertex_filter(None)
 
     # b = time.time()
 
     # print "Finished at ", datetime.datetime.now(), ","
     # print "Runtime: ", (b-a)/sampling_size
 
-    # return list(motif_counts)
+    return list(motif_counts)
 
--- test/test_three_motifs.py
+++ test/test_three_motifs.py
@@ -3,19 +3,32 @@
 import unittest, os
 
 import graph_tool as gt
+import numpy as np
 
-from comp.functions import ( sample_three_motifs )
+from comp.functions import ( sample_three_motifs,
+                             generate_three_motif_list )
 
 
 class Test_sample_three_motifs(unittest.TestCase):
 
-    g = gt.Graph()
-    g.add_vertex(3)
-    g.add_edge(g.vertex(0), g.vertex(1))
-    g.add_edge(g.vertex(1), g.vertex(2))
-    g.add_edge(g.vertex(2), g.vertex(0))
+    # g = gt.Graph()
+    # g.add_vertex(5)
+    # g.add_edge(g.vertex(0), g.vertex(1))
+    # g.add_edge(g.vertex(1), g.vertex(2))
+    # g.add_edge(g.vertex(2), g.vertex(0))
 
-    sample_three_motifs(g, 10)
+    # # print(gt.topology.isomorphism(g, g))
+    # x = sample_three_motifs(g, 20)
+    # # print( x )
+
+    def test_single_matches_from_motif_list(self):
+        three_motif_list = generate_three_motif_list(with_empty=True)
+    
+        for k,motif in enumerate(three_motif_list):
+            matches = sample_three_motifs(motif,1)
+            self.assertEqual(int(np.where(np.array(matches)==1)[0]),
+                             k)
+
 
 
 if __name__ == '__main__':
