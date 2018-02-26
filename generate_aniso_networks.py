060c789aa7f70021e9922ee44a089ae9d2265145
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2018-02-26 19:11:24 +0100

    count graphs instead uuid

--- generate_aniso_networks.py
+++ generate_aniso_networks.py
@@ -4,14 +4,14 @@ from params.generate_aniso_networks_params import *
 from generate_aniso_networks_label import label
 
 import numpy as np
-import uuid
 
 np.random.seed(seed)
 
 w_f = lambda x: w
 
+k = 0
 for i in range(n_graphs):
     spath = "/home/lab/comp/data/" + label \
-            +"-"+str(uuid.uuid4())[:4]+".gt"
+            +"-"+str(k)+".gt"
     generate_aniso_network(N, w_f, ed_l, spath)
                            
