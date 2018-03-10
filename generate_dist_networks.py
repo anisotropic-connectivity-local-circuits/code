39c83f4b2a26effa71d53bf3332cd6dcd0eddbda
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2018-03-10 20:42:03 +0100

    add seed & change to numbered naming scheme

--- generate_dist_networks.py
+++ generate_dist_networks.py
@@ -4,13 +4,14 @@ from functions import ( generate_dist_network,
 from params.generate_dist_networks_params import *
 from generate_dist_networks_label import label
 
+import numpy as np
 
-import uuid
+np.random.seed(seed)
 
 an_ddcp = Aniso_netw_dist_profile(w)
 
 for i in range(n_graphs):
     spath = "/home/lab/comp/data/" + label \
-            +"-"+str(uuid.uuid4())[:4]+".gt"
+            +"-"+"%.2d" % i +".gt"
     generate_dist_network(N, an_ddcp.C, ed_l, spath)
                            
