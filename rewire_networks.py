144e64f1e5059cd267dc75dba48d32d782ed0b88
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2018-03-10 19:29:36 +0100

    update for different graph name input & add seed

--- rewire_networks.py
+++ rewire_networks.py
@@ -6,9 +6,13 @@ from rewire_networks_label import label
 import graph_tool as gt
 import sys, uuid, os, pickle
 
+import numpy as np
+
+np.random.seed(seed)
+
 
 for gpath in sys.argv[1:-1]:
-    gid = os.path.splitext(gpath)[0][-4:]
+    gid = os.path.splitext(gpath)[0][-2:]
     g = gt.load_graph(gpath)
     h, stat = rewire(g, rew_frac, eps_f)
     spath = "/home/lab/comp/data/"+label+"-"+gid
@@ -16,8 +20,4 @@ for gpath in sys.argv[1:-1]:
     with open(spath+"_stat.p", "wb") as pfile:
         pickle.dump(stat,pfile)
 
-
-# for i in range(n_graphs):
-#     spath = "/home/lab/data/"+label+"-"+str(uuid.uuid4())[:4]+".gt"
-#     rewire(g, rew_frac=rew_frac, eps_f = eps_f)
                            
