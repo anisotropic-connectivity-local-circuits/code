38de94fefff59c2327fdbf9af88f4cf1f6fbe516
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2018-03-11 09:46:24 +0100

    numbered labeling & random seed

--- sample_three_motifs.py
+++ sample_three_motifs.py
@@ -7,11 +7,15 @@ from sample_three_motifs_label import label
 
 import pickle, os, sys
 
+import numpy as np
+
+np.random.seed(seed)
+
 data = {}
 for gpath in sys.argv[1:-1]:
     g = gt.load_graph(gpath)
     motif_counts = sample_three_motifs(g, sample_size)
-    gid = os.path.splitext(os.path.basename(gpath))[0][-4:]
+    gid = os.path.splitext(os.path.basename(gpath))[0][-2:]
 
     data[gid]=motif_counts
 
