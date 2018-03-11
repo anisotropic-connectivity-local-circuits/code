71956359c3f83dbcadafec90f659ece4b921965f
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2018-03-11 09:33:04 +0100

    numbered label & seed

--- generate_tuned_networks.py
+++ generate_tuned_networks.py
@@ -4,13 +4,16 @@ from functions import ( generate_aniso_network,
 from params.generate_tuned_networks_params import *
 from generate_tuned_networks_label import label
 
+import numpy as np
 
-import uuid
+
+np.random.seed(seed)
+
 
 tn_ddcp = Tuned_netw_dist_profile()
 
 for i in range(n_graphs):
     spath = "/home/lab/comp/data/" + label \
-            +"-"+str(uuid.uuid4())[:4]+".gt"
+            +"-"+"%.2d" % i +".gt"
     generate_aniso_network(N, tn_ddcp.C, ed_l, spath)
                            
